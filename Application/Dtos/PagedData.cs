71739fb19b52ada15e64279bc6225d95bac46e38
habibfci2014 <111754787+habibfci2014@users.noreply.github.com>
2024-09-23 06:04:27 +0300

    Delete Application directory

--- Application/Dtos/PagedData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Dtos;
-
-public class PagedData<T>
-{
-    public List<T> Result { get; set; } = new List<T>();
-    public int TotalRecords { get; set; }
-    public int LastPage { get; set; }
-    public int CurrentPage { get; set; }
-}
